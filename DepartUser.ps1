ae8729744c09200f059c66d63bae62a2cb00190f
jaywangpeng <jaywangpeng@hotmail.com>
2016-10-28 18:01:37 +1100

    Add files via upload

---
+++ DepartUser.ps1
@@ -0,0 +1,8 @@
+﻿#Global variables, Constants
+$HomeDirBase="\\fileshares\UserData\Homedirs\"
+$DesktopBase="\\fileshares\UserData\desktops\"
+$MyDocumentsBase="\\fileshares\UserData\Documents\"
+
+#Functions
+
+#
