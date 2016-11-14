d22381c0d760b1b645cc742d83025f2c308d7b24
jaywangpeng <jaywangpeng@hotmail.com>
2016-11-15 07:50:03 +1100

    Add files via upload

--- AddGroupsToUser.ps1
+++ AddGroupsToUser.ps1
@@ -1,5 +1,13 @@
+Import-Module ActiveDirectory
+
 #RefUser is the one who you want to check what groups he/she belongs to.
-$RefUser = Read-Host "What is the user's Full Name?"
-$User = Get-ADUser -Identity $RefUser -Properties *
-$GroupMemberShip = ($User.Memberof | %{(Get-ADGroup $_).Name;}) -Join ';'
-$GroupMemberShip 
+$RefUserInput = Read-Host "What is the account of the referenced user(e.g. jwang)?"
+$RefUser = Get-ADUser -Identity $RefUserInput -Properties SAMAccountName,Memberof
+
+#TargetUser is the one who you are adding to groups.
+$TargetUserInput = Read-Host "What is the account of the user you want to add?"
+$TargetUser = Get-ADUser -Identity $TargetUserInput -Properties SAMAccountName,Memberof
+
+#Add the target user to the ref user's Memberof groups
+$GroupMemberShip = ($RefUser.Memberof | Foreach-Object {(Get-ADGroup $_).Name;})
+$GroupMemberShip | Foreach-Object {Add-ADGroupMember -Identity $_ -Member $TargetUser.SAMAccountName}
