66bb8e3ffbb4186f35639358ac0214d59fa2fec9
Jay Wang <jaywangpeng@hotmail.com>
2017-10-13 08:23:29 +1100

    Useful scripts
    a script to send email and the other one is to delete IIS logs on multiple servers.

---
+++ DeleteIISLog.ps1
@@ -0,0 +1,20 @@
+﻿# This script is to delete IIS log files older than 30 days
+# on the servers in servers.txt
+
+$Servers = Get-Content 'C:\ScheduledTasks\DeleteIISLog\Servers.txt'
+
+foreach ($Server in $Servers)
+{
+    $LogFiles = Get-ChildItem "\\$Server\C$\inetpub\logs\LogFiles\*.log" `
+        -Recurse -ErrorAction Stop
+    foreach ($LogFile in $LogFiles)
+    {
+        if([datetime]::ParseExact(
+            $LogFile.BaseName.TrimStart('u_ex', 'u_in'), 'yyMMd',
+            [System.Globalization.CultureInfo]::CurrentCulture) `
+            -lt (Get-Date).AddDays(-31))
+        {
+            Remove-Item $LogFile
+        }
+    }
+}
